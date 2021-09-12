0580c3176c51c08fc92f40af02aebd785f45f784
KPMGE <jesuskevin254@gmail.com>
2021-09-12 14:30:25 -0300

    Criando componentes para renderizar lista de plantas

---
+++ src/components/Card/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import { styles } from "./styles";
+
+import { Fontisto } from "@expo/vector-icons";
+import { theme } from "../../global/styles/theme";
+import plantIcon from "../../assets/fotoPlantinha.png";
+
+export const Card = ({ plantName }) => {
+  const hello = () => {
+    alert("hello");
+  };
+
+  return (
+    <View style={styles.container}>
+      <Image source={plantIcon} style={styles.image} />
+
+      <View style={styles.content}>
+        <Text style={styles.text}>{plantName}</Text>
+        <View style={styles.line} />
+      </View>
+
+      <TouchableOpacity style={styles.button} onPress={hello}>
+        <Fontisto name="more-v-a" size={24} color={theme.colors.lightGray} />
+      </TouchableOpacity>
+    </View>
+  );
+};
